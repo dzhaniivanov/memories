90357f29302ab1866534e8c1d64571344cf1d058
dzhani ivanov <djani.ivanov94@gmail.com>
2021-07-26 20:52:04 +0300

    post model added

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,10 +1,10 @@
 import express from 'express';
 
+import { getPosts } from '../controllers/posts.js'
+
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('this works');
-});
+router.get('/', getPosts);
 
 
 export default router;
