6cc8bfbc23a75e27eec9b47840ea483dbad553b4
dzhani ivanov <djani.ivanov94@gmail.com>
2021-07-27 23:12:17 +0300

    deployed backend to heroku,frontend to netlify

--- server/index.js
+++ server/index.js
@@ -17,8 +17,10 @@ app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use('/posts', postRoutes);
+app.get('/', (req, res) => {
+    res.send('Hello to Memories API!')
+});
 
-//const CONNECTION_URL = 'mongodb+srv://memories:memories123@memories-cluster.rg39c.mongodb.net/memories-cluster?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
