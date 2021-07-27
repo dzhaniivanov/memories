3a9566662f137b72216a77029fbecd60f463c68c
dzhani ivanov <djani.ivanov94@gmail.com>
2021-07-27 22:30:08 +0300

    environmental variables added

--- server/index.js
+++ server/index.js
@@ -2,12 +2,14 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
 
 
 
 const app = express();
+dotenv.config();
 
 
 app.use(express.json({ limit: "30mb", extended: true }));
@@ -16,10 +18,10 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://memories:memories123@memories-cluster.rg39c.mongodb.net/memories-cluster?retryWrites=true&w=majority';
+//const CONNECTION_URL = 'mongodb+srv://memories:memories123@memories-cluster.rg39c.mongodb.net/memories-cluster?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server is running on port ${PORT}!`)))
     .catch((error) => console.log(error.message))
 
