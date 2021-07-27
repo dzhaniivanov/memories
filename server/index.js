f9de4900fe2bef14c2d5cf2198872c5905c4d722
dzhani ivanov <djani.ivanov94@gmail.com>
2021-07-27 18:25:22 +0300

    posts and post component are working

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ const CONNECTION_URL = 'mongodb+srv://memories:memories123@memories-cluster.rg39
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
+    .then(() => app.listen(PORT, () => console.log(`Server is running on port ${PORT}!`)))
     .catch((error) => console.log(error.message))
 
 mongoose.set('useFindAndModify', false)
