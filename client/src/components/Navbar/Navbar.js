b05e6375c8513882afce81460892c40701a8aa76
dzhani ivanov <djani.ivanov94@gmail.com>
2021-07-28 13:57:07 +0300

    making auth reducer

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { AppBar, Toolbar, Typography, Avatar, Button } from '@material-ui/core';
 import useStyles from './styles';
@@ -8,8 +8,7 @@ import memories from '../../images/memories.png';
 const Navbar = () => {
     const classes = useStyles();
 
-    const user = null;
-
+    const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
 
     return (
         <AppBar className={classes.appBar} position="static" color="inherit">
@@ -27,7 +26,7 @@ const Navbar = () => {
                         >
                             {user.result.name.charAt(0)}
                         </Avatar>
-                        <Typography className={classes.username} variant="h6">{user.result.name}</Typography>
+                        <Typography className={classes.userName} variant="h6">{user.result.name}</Typography>
                         <Button variant="contained" className={classes.logout} color="secondary">Logout</Button>
                     </div>
                 ) : (
@@ -35,7 +34,7 @@ const Navbar = () => {
                 )}
             </Toolbar>
         </AppBar>
-    )
-}
+    );
+};
 
 export default Navbar
