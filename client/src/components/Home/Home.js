b1313f230ef087138d8a900f18b463832120e6a9
dzhani ivanov <djani.ivanov94@gmail.com>
2021-07-31 17:49:46 +0300

    start working on pagination

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Grow, Grid } from '@material-ui/core';
+import { Container, Grow, Grid, Paper } from '@material-ui/core';
 import Posts from '../Posts/Posts';
 import Form from '../Form/Form';
 
 import { getPosts } from '../../actions/posts';
+import Pagination from '../Pagination';
 import { useDispatch } from 'react-redux';
 import useStyles from '../../styles';
 
@@ -26,6 +27,9 @@ const Home = () => {
                     </Grid>
                     <Grid item xs={12} sm={4}>
                         <Form currentId={currentId} setCurrentId={setCurrentId} />
+                        <Paper elevation={6}>
+                            <Pagination />
+                        </Paper>
                     </Grid>
                 </Grid>
             </Container>
