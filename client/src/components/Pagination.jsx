462143e2c947e5c7865a1605f2fba0588b85a40c
dzhani ivanov <djani.ivanov94@gmail.com>
2021-08-05 09:28:23 +0300

    pagination works

--- client/src/components/Pagination.jsx
+++ client/src/components/Pagination.jsx
@@ -7,6 +7,8 @@ import { getPosts } from "../actions/posts";
 import useStyles from "./styles";
 
 const Paginate = ({ page }) => {
+  const { numberOfPages } = useSelector((state) => state.posts);
+
   const classes = useStyles();
   const dispatch = useDispatch();
 
@@ -14,16 +16,19 @@ const Paginate = ({ page }) => {
     if (page) dispatch(getPosts(page));
   }, [page]);
 
-
   return (
     <Pagination
       classes={{ ul: classes.ul }}
-      count={5}
-      page={1}
+      count={numberOfPages}
+      page={Number(page) || 1}
       variant="outlined"
       color="primary"
       renderItem={(item) => (
-        <PaginationItem {...item} component={Link} to={`/posts?page=${1}`} />
+        <PaginationItem
+          {...item}
+          component={Link}
+          to={`/posts?page=${item.page}`}
+        />
       )}
     />
   );
