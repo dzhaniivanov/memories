3df70d6d05b0bbe6ab9c4fdd4a8fa7f0b4b178c4
dzhani ivanov <djani.ivanov94@gmail.com>
2021-08-13 15:50:29 +0300

    ...

--- client/src/components/PostDetails/PostDetails.jsx
+++ client/src/components/PostDetails/PostDetails.jsx
@@ -25,13 +25,13 @@ const PostDetails = () => {
     dispatch(getPost(id));
   }, [id]);
 
-  useEffect(() => {
-    if (post) {
-      dispatch(
-        getPostsBySearch({ search: "none", tags: post?.tags.join(",") })
-      );
-    }
-  }, [post]);
+  /*   useEffect(() => {
+      if (post) {
+        dispatch(
+          getPostsBySearch({ search: "none", tags: post?.tags.join(",") })
+        );
+      }
+    }, [post]); */
 
   if (!post) return null;
 
